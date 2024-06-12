4028ca4081d7ea8ac0078acffa3ffc6d64e12706
Aruthra S <aruthras06@gmail.com>
2024-06-12 14:16:03 +0530

    Fixed: origin issue

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 const port = 3000;
 
 app.use(express.json());
