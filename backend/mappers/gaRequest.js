24aa8648db4bc0725ea935db58a426cb22ed14bf
Aruthra S <aruthras06@gmail.com>
2024-06-12 12:14:47 +0530

    Added : Filters

--- backend/mappers/gaRequest.js
+++ backend/mappers/gaRequest.js
@@ -1,18 +1,34 @@
 function gaRequest(data){
     var param = {};
     for(let key in data){
-        if(key != "dateRanges"){
+        if(key!="dateRanges" && key!="dimensionFilter" && key!="metricsFilter"){
             param[key] = data[key].map((item) => {
                 return { name: item };
             });
         }
-        else{
+        else if(key == "dateRanges"){
             param[key] = data[key].map((item)=>{
                 return {
                     startDate: item[0],
                     endDate: item[1]
                 }
             })
+        }
+        else{
+            param[key] = {
+              andGroup: {
+                expressions: data[key].map((item) => {
+                  return {
+                    filter: {
+                      fieldName: item.fieldName,
+                      inListFilter: {
+                        values: item.filters,
+                      },
+                    },
+                  };
+                }),
+              },
+            };
         }
     }
     return param;   
