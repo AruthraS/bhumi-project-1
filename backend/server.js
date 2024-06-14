0388d355c227f4f1067056bb25f7812724c0126d
Raviprasath K J <raviprasath320@gmail.com>
2024-06-14 09:03:48 +0530

    hover

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 3000;
 
 app.listen(port,() => {
-    console.log(`server is running in the port :${port}`};
+    console.log(`server is running in the port : ${port}`);
 }) ;
