6ec94b3c1862d13001f836373ee2d021ed0f02ee
Aruthra S <aruthras06@gmail.com>
2024-06-14 14:04:50 +0530

    Completed: RealTimeReport

--- backend/routers/AnalyticsRouter/router.js
+++ backend/routers/AnalyticsRouter/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 // Controllers
 const ReportController = require('./Controllers/ReportController');
+const RealTimeReport = require('./Controllers/RealTimeReport');
 // 
 
 router.post('/report',ReportController);
+router.post('/realtime',RealTimeReport);
 
 module.exports = router;
