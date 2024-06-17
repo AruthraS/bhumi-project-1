b5a1c2a146efb7ea804faffad05cd0d9ac61efd6
Aruthra S <aruthras06@gmail.com>
2024-06-17 11:03:14 +0530

    Added: MinutesAgo in gaRequest mapper

--- backend/mappers/gaRequest.js
+++ backend/mappers/gaRequest.js
@@ -1,34 +1,45 @@
 function gaRequest(data){
     var param = {};
     for(let key in data){
-        if(key!="dateRanges" && key!="dimensionFilter" && key!="metricsFilter"){
-            param[key] = data[key].map((item) => {
-                return { name: item };
-            });
-        }
-        else if(key == "dateRanges"){
-            param[key] = data[key].map((item)=>{
-                return {
-                    startDate: item[0],
-                    endDate: item[1]
-                }
-            })
-        }
-        else{
-            param[key] = {
-              andGroup: {
-                expressions: data[key].map((item) => {
-                  return {
-                    filter: {
-                      fieldName: item.fieldName,
-                      inListFilter: {
-                        values: item.filters,
-                      },
-                    },
-                  };
-                }),
-              },
+        if (
+          key != "dateRanges" &&
+          key != "dimensionFilter" &&
+          key != "metricsFilter" &&
+          key != "minuteRanges"
+        ) {
+          param[key] = data[key].map((item) => {
+            return { name: item };
+          });
+        } else if (key == "dateRanges") {
+          param[key] = data[key].map((item) => {
+            return {
+              startDate: item[0],
+              endDate: item[1],
             };
+          });
+        } else if (key == "minuteRanges") {
+          param[key] = data[key].map((item) => {
+            return {
+              startMinutesAgo: item[0],
+              endMinutesAgo: item[1],
+            };
+          });
+        }
+        else {
+          param[key] = {
+            andGroup: {
+              expressions: data[key].map((item) => {
+                return {
+                  filter: {
+                    fieldName: item.fieldName,
+                    inListFilter: {
+                      values: item.filters,
+                    },
+                  },
+                };
+              }),
+            },
+          };
         }
     }
     return param;   
